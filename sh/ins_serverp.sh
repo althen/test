a0e5a4ff6bab8c599eb697d4ce070c6b01feaf46
jiming.song <jiming.song@yunzhanghu.com>
2021-12-25 10:24:30 +0800

    ins serverp

--- sh/ins_serverp.sh
+++ sh/ins_serverp.sh
@@ -1,13 +1,13 @@
 #! /bin/bash
 sudo apt update
 sudo apt install wget tar -y
-url="https://raw.githubusercontent.com/althen/test/v1/sh/"
+url="https://raw.githubusercontent.com/althen/test/v1/sh"
 file="serverp.tar"
 
 rm -rf ~/httpserver
 mkdir ~/httpserver
 
-wget "${url}/${file}" -O "~/httpserver/${file}"
+wget "${url}/${file}" -O ~/httpserver/${file}
 cd ~/httpserver
 ls -l
 
