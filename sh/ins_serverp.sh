da17e7916e28c79be201e9226e4f29d4bde55f78
Ubuntu <ubuntu@ip-172-31-24-119.ap-northeast-2.compute.internal>
2023-04-08 05:57:21 +0000

    server cert

--- sh/ins_serverp.sh
+++ sh/ins_serverp.sh
@@ -1,6 +1,11 @@
 #! /bin/bash
-sudo apt update
-sudo apt install wget tar -y
+rm -rf ~/httpserver
+mkdir ~/httpserver
+
+tar -zvxf srv_cert.tar -C httpserver
+
+#sudo apt update
+#sudo apt install wget tar -y
 url="https://raw.githubusercontent.com/althen/test/v1/sh"
 filebin="serverp"
 
@@ -16,14 +21,13 @@ filebin="serverp"
 
 
 
-rm -rf ~/httpserver
-mkdir ~/httpserver
+#wget "${url}/${file}" -O ~/httpserver/${file}
+curl  -o ~/httpserver/${file} "${url}/${file}"
 
-wget "${url}/${file}" -O ~/httpserver/${file}
 cd ~/httpserver
 ls -l
 
 tar -xzvf ./$file
 chmod 700 ./$file
-./$filebin
+./$filebin -port=7791
 
